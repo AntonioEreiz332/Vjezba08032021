78bdd05c1b3d46830f4e78b145deb8c8cdef7010
AntonioEreiz332 <58472078+AntonioEreiz332@users.noreply.github.com>
2021-03-08 09:17:48 +0100

    Add files via upload

---
+++ zad08032021/zad08032021/Brkovi.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad08032021
+{
+    class Brkovi
+    {
+        string ime, prezime,spol, dodatak;
+        int god;
+
+        public void setIme(string ime) {
+            this.ime = ime;
+
+        }
+        public string getIme()
+        {
+            return this.ime;
+        }
+        //prezime
+        public void setPrezime(string prezime)
+        {
+            this.prezime = prezime;
+        }
+        public string getPrezime()
+        {
+            return this.prezime;
+        }
+        //god
+        public void setGod(int god)
+        {
+            this.god = god;
+        }
+        public int getGod() {
+            return this.god;
+        }
+        //spol
+        public void setSpol(string spol){
+            this.spol = spol;        
+        }
+        public string getSpol()
+        {
+            return this.spol;
+        }
+        //dodatak
+        public void setDodatak(string dodatak)
+        {
+            this.dodatak = dodatak;
+        }
+        public string getDodatak()
+        {
+            return this.dodatak;
+        }
+
+        public Brkovi(string ime,string prezime,int god,string spol)
+        {
+            this.ime = ime;
+            this.prezime = prezime;
+            this.god = god;
+            this.spol = spol;
+        }
+        public string Ispis()
+        {
+            string ispis = ime + "\t" + prezime + "\t"
+                + Convert.ToString(god) + "\t"
+                + spol + "\t"+ dodatak + "\r\n";
+            return ispis;
+        }
+
+
+
+
+
+    }
+}
---
+++ zad08032021/zad08032021/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zad08032021
+{
+    public partial class Form1 : Form
+    {
+        List<Brkovi> lista = new List<Brkovi>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnUnesi_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                Brkovi brk = new Brkovi(textBox1.Text, textBox2.Text,
+                    Convert.ToInt32(textBox3.Text),
+                    Convert.ToString(comboBox1.Text));
+               
+                lista.Add(brk);
+
+                
+                textBox1.Clear();
+                textBox2.Clear();
+                textBox3.Clear();
+            }
+            catch (Exception greska)
+            {
+               
+                MessageBox.Show("Pogrešan unos!\r\n" + greska.Message,
+                    "Greška", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnObradi_Click(object sender, EventArgs e)
+        {
+            foreach (Brkovi brk in lista)
+            {
+                
+                if (brk.getSpol() == "M")
+                {
+                    brk.setDodatak("Ima brkove");
+                }
+                else
+                {
+                    brk.setDodatak("Nema brkove.");
+                }
+
+            }
+        }
+            private void btnIspis_Click(object sender, EventArgs e)
+        {
+                richtxtIspis.Text = "Podatak 1" + "\tPodatak 2"
+                    + "\tPodatak 3" + "\tPodatak 4" + "\tDodatak\r\n";
+                foreach (Brkovi brk in lista)
+                {
+                    richtxtIspis.AppendText(brk.Ispis());
+                }
+            }
+    }
+}
